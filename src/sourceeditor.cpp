2f6ecca99a446b3f7dcac37c7b9dfcac944f522e
Oleg G <verdrehung@gmail.com>
2012-04-14 17:37:59 +0700

    make SourceEditor QsciScintilla child and utf-8 enabled

--- src/sourceeditor.cpp
+++ src/sourceeditor.cpp
@@ -1,32 +1,28 @@
 #include "sourceeditor.h"
 
-SourceEditor::SourceEditor(QWidget *parent) :
-    QWidget(parent)
+SourceEditor::SourceEditor(QsciScintilla *parent) :
+    QsciScintilla(parent)
 {
-    QVBoxLayout *layout = new QVBoxLayout(this);
+    setLexer(new QsciLexerPerl(this)); // only Perl for now :(
+    setAutoIndent(true);
+    setMarginLineNumbers(NumberMargin, true);
+    setMarginWidth(NumberMargin, "3 ");
+    setBraceMatching(StrictBraceMatch);
+    setUtf8(true);
 
-    QToolBar *toolBar = new QToolBar;
-    layout->addWidget(toolBar);
-
-    m_sourceEdit = new QsciScintilla;
-    m_sourceEdit->setLexer(new QsciLexerPerl(m_sourceEdit)); // only Perl for now :(
-    m_sourceEdit->setAutoIndent(true);
-    m_sourceEdit->setMarginLineNumbers(QsciScintilla::NumberMargin, true);
-    m_sourceEdit->setMarginWidth(QsciScintilla::NumberMargin, "3 ");
     m_marginWidth = 1;
-    connect(m_sourceEdit, SIGNAL(linesChanged()), this, SLOT(linesCountChanged()));
-    layout->addWidget(m_sourceEdit);
+    connect(this, SIGNAL(linesChanged()), this, SLOT(linesCountChanged()));
 }
 
 void SourceEditor::linesCountChanged()
 {
-    int marginWidth = qCeil(qLn(m_sourceEdit->lines()) / qLn(10) + 0.000001);
+    int marginWidth = qCeil( qLn( lines() ) / qLn(10) + 0.000001 );
     if (marginWidth == 0) {
         marginWidth++;
     }
 
     if (marginWidth != m_marginWidth) {
         m_marginWidth = marginWidth;
-        m_sourceEdit->setMarginWidth(QsciScintilla::NumberMargin, QString(marginWidth, '3') + ' ');
+        setMarginWidth(NumberMargin, QString(marginWidth, '3') + ' ');
     }
 }
--- src/sourceeditor.h
+++ src/sourceeditor.h
@@ -1,24 +1,19 @@
 #ifndef SOURCEEDITOR_H
 #define SOURCEEDITOR_H
 
-#include <QWidget>
-#include <QVBoxLayout>
-#include <QToolBar>
-#include <QFont>
 #include <QtCore/qmath.h>
 #include <Qsci/qsciscintilla.h>
 #include <Qsci/qscilexerperl.h>
 
 #include <QDebug>
 
-class SourceEditor : public QWidget
+class SourceEditor : public QsciScintilla
 {
     Q_OBJECT
 public:
-    explicit SourceEditor(QWidget *parent = 0);
+    explicit SourceEditor(QsciScintilla *parent = 0);
 
 protected:
-    QsciScintilla *m_sourceEdit;
     int m_marginWidth;
 
 protected slots:
